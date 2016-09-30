12cb3dd0176955c2668806b445e14d79dcb27330
Avichay Attlan <avichay13@gmail.com>
2016-09-30 14:02:42 +0300

    First

---
+++ src/main/java/aaa/aaa/App.java
@@ -0,0 +1,39 @@
+package aaa.aaa;
+
+/**
+ * Hello world!
+ *
+ */
+import static spark.Spark.*;
+import static aaa.aaa.JsonUtil.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class App {
+
+    private static final String template = "Hello, %s!";
+    private final static AtomicLong counter = new AtomicLong();
+    private static GreetingService greetingService = new GreetingService();
+    
+    public static void main(String[] args) {
+    	port(5000);
+    	
+    	get("/greetings", (req, res) -> greetingService.getAllUsers(), json());
+        
+        get("/string", (req, res) -> {
+        	String string = "String!";
+        	return string;
+        });
+        
+        get("/greeting/:name", (req, res) -> {
+        	String name = req.params(":name");
+        	Greeting greeting = new Greeting(counter.incrementAndGet(),
+                    String.format(template, name));
+        	return greeting;
+        }, json());
+        
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+    }
+}
